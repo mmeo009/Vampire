ab447256f7aba6d6389855c8f7c29b20f57c190a
mmeo009 <shh020615@gmail.com>
2024-04-18 06:19:50 +0900

    [feat] 몬스터 111112A, 111113A 구현 후 웨이브에 따른 몬스터 생성

--- BattleNoid_V2/Assets/Scripts/Controller/MonsterController.cs
+++ BattleNoid_V2/Assets/Scripts/Controller/MonsterController.cs
@@ -10,31 +10,140 @@ public class MonsterController : MonoBehaviour
     [SerializeField] private MonsterStats monster;
     [SerializeField] private Rigidbody monsterRigidbody;
     [SerializeField] private GameObject attackPivot;
+    [SerializeField] private float knockBackTimer;
+    [SerializeField] private float attackTimer;
+    [SerializeField] private bool isFreeze = false;
 
     public PlayerController Player;
-    public void OnEnable()
+    public float freezeTimer;
+
+    private void OnEnable()
     {
         monster = null;
     }
-    public void Update()
+    private void Update()
     {
         Move();
+
+        if(freezeTimer > 0)
+        {
+            freezeTimer -= Time.deltaTime;
+            if(freezeTimer <= 0)
+            {
+                isFreeze = false;
+            }
+        }
+
+        if (attackTimer > 0f)
+        {
+            attackTimer -= Time.deltaTime;
+        }
+
+        if(this.transform.position.y != 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            BulletController bullet = other.GetComponent<BulletController>();
+            ChangeMonsterStats(OperationType.Minus, StatType.CurrentHP, bullet.damage);
+            bullet.DestroyBullet();
+        }
+        else if(other.CompareTag("Monster"))
+        {
+            Physics.IgnoreCollision(other, other);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && attackTimer <= 0f)
+        {
+            Managers.Player.SetStats(OperationType.Minus, StatType.CurrentHP, monster.attackDamage);
+            attackTimer = monster.attackSpeed;
+            knockBackTimer = monster.knockBackTime;
+        }
+    }
+
+    private void Move()
+    {
+        if(monster != null)
+        {
+            float moveSpeed = monster.moveSpeed;
+
+            if (knockBackTimer > 0)
+            {
+                knockBackTimer -= Time.deltaTime;
+
+                if (moveSpeed > 0)
+                {
+                    moveSpeed = -moveSpeed * monster.knockBackAmount;
+                }
+
+                if (knockBackTimer < 0)
+                {
+                    moveSpeed = Mathf.Abs(moveSpeed * 0.5f);
+                }
+            }
+
+            if (Vector3.Distance(Player.transform.position, transform.position) > monster.attackRange)
+            {
+                monsterRigidbody.velocity = (Player.transform.position - transform.position).normalized * moveSpeed;
+            }
+            else
+            {
+                Attack();
+            }
+
+            if (isFreeze)
+            {
+                moveSpeed = 0;
+            }
+
+            Vector3 targetDiraction = (Player.transform.position - this.transform.position).normalized;
+            Quaternion targetRotation = Quaternion.LookRotation(targetDiraction);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, monster.rotationSpeed * Time.deltaTime);
+        }
+    }
+    private void Attack()
+    {
+        if (monster.attackType == 2)
+        {
+            attackPivot.GetComponent<Transform>();
+            //TODO ÃÑ¾Ë »§¾ß Ãß°¡
+        }
+    }
+
+    public void Freeze(int amount)
+    {
+        isFreeze = true;
+        freezeTimer = amount;
+    }
+
+
     public void GetMonsterStats(MonsterStats _monster)
     {
         Player = Managers.Player.player.playerController;
         monsterRigidbody = GetComponent<Rigidbody>();
-        attackPivot = Util.FindChild(this.gameObject, "AttackPivot");
+
         if (monster == null)
         {
             monster = _monster;
         }
+
+        if (monster.attackType == 2)
+        {
+            attackPivot = Util.FindChild(this.gameObject, "AttackPivot");
+        }
     }
     public void ChangeMonsterStats(OperationType operation, StatType stat, float amount = 0, MonsterStats ms = null)
     {
-        if(operation == OperationType.Plus)
+        if (operation == OperationType.Plus)
         {
-            switch(stat)
+            switch (stat)
             {
                 case StatType.CurrentHP:
                     monster.hp += amount;
@@ -47,18 +156,18 @@ public class MonsterController : MonoBehaviour
                     break;
             }
         }
-        else if(operation == OperationType.Minus) 
+        else if (operation == OperationType.Minus)
         {
             switch (stat)
             {
                 case StatType.CurrentHP:
                     monster.hp -= amount;
-                    if(monster.hp <= 0)
+                    if (monster.hp <= 0)
                         MonsterDie();
                     break;
                 case StatType.AttackDamage:
                     monster.attackDamage += amount;
-                    if(monster.attackDamage <= 0)
+                    if (monster.attackDamage <= 0)
                         monster.attackDamage = 0;
                     break;
                 case StatType.MoveSpeed:
@@ -68,44 +177,6 @@ public class MonsterController : MonoBehaviour
         }
 
     }
-
-    public void Move()
-    {
-        if(monster != null)
-        {
-            Vector3 direction = (Player.transform.position - this.transform.position).normalized;
-
-            monsterRigidbody.MovePosition(this.transform.position + direction * monster.moveSpeed * Time.deltaTime);
-
-            Vector3 targetDiraction = (Player.transform.position - this.transform.position).normalized;
-
-            Quaternion targetRotation = Quaternion.LookRotation(targetDiraction);
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, monster.rotationSpeed * Time.deltaTime);
-        }
-    }
-    public void Attack()
-    {
-        if(monster.code == "111111A")
-        {
-            attackPivot.GetComponent<SphereCollider>()  ;
-        }
-    }
-
-    private void GetDmg(float amount)
-    {
-        ChangeMonsterStats(OperationType.Minus,StatType.CurrentHP, amount);
-    }
-
-    public void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("Bullet"))
-        {
-            BulletController bullet = other.GetComponent<BulletController>();
-            GetDmg(bullet.damage);
-            bullet.DestroyBullet();
-        }
-    }
-
     public void MonsterDie()
     {
         Managers.Monster.monsters.Remove(this as MonsterController);
