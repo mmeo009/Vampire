d40f97fe7d9e73d99bcb31f8a1d08b0856e69d2d
mmeo009 <shh020615@gmail.com>
2024-04-26 01:04:17 +0900

    [feat] 카드 사용법

--- BattleNoid_V2/Assets/Scripts/Controller/CardController.cs
+++ BattleNoid_V2/Assets/Scripts/Controller/CardController.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class CardController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private CardData myData;
+    [SerializeField] private TMP_Text perkName;
+    [SerializeField] private TMP_Text perkStats;
+    [SerializeField] private Button button;
+    void Awake()
     {
-        
-    }
+        myData = Managers.Card.GetCard();
 
-    // Update is called once per frame
-    void Update()
+        if(myData != null)
+        {
+            perkName.text = myData.cardName;
+            string cardTesks = "";
+            foreach(CardTesk card in myData.cardTesks)
+            {
+                cardTesks += $"{card.stat}¸¦ {card.amount}¸¸Å­ {card.operationType}\r\n";
+            }
+            perkStats.text = cardTesks;
+
+            AddButtonTesk();
+        }
+    }
+    private void AddButtonTesk()
     {
-        
+        button.onClick.AddListener(() => Managers.Card.UseMethod(myData));
     }
 }
